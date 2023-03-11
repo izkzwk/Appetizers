ec620b99d4700e0f5e29e93ebdc40b27acfdfdfa
Dzmitry Bladyka <dzmitrydistortion@gmail.com>
2023-03-12 02:52:17 +0300

    Initial Commit

---
+++ Appetizers/AppetizersApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppetizersApp.swift
+//  Appetizers
+//
+//  Created by Dzmitry Bladyka on 12.03.23.
+//
+
+import SwiftUI
+
+@main
+struct AppetizersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
